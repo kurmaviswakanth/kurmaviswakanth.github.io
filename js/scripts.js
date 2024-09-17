ced0cad4c6266971eeed89bf9ac565824ce11339
KURMA VISWAKANTH <kviswakanth@gmail.com>
2024-09-18 00:53:21 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+/*!
+* Custom JavaScript for your project
+* Add your custom scripts here
+*/
+
+// Smooth Scroll for Anchor Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Initialize Bootstrap Tooltips
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
+
+// Toggle Mobile Menu
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.querySelector('.navbar-toggler');
+    var menuCollapse = document.querySelector('.navbar-collapse');
+    
+    menuToggle.addEventListener('click', function() {
+        menuCollapse.classList.toggle('show');
+    });
+});
